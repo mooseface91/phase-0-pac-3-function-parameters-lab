1aaea56032c60310ca3ff1cc3141734cd99ae3a3
joseenriquez <co.hiker.dude@gmail.com>
2022-10-18 07:56:07 -0600

    commit tree 14d6d2fe21176429f1f595cc40ffc11d2a8f7a9e

---
+++ index.js
@@ -0,0 +1,11 @@
+const name = Aki 
+function introduction(name) {
+    return (`Hi, my name is ${name}.`);
+}
+const language = Ember.js
+function introductionWithLanguage(name, language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
